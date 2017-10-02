a1c7d40b09521a08de550b8a1831cf6ca0784faa
Jamal Hasuev <metafra@mail.ru>
2017-10-02 14:28:29 +0300

    Модифицировал блок Обо мне и добавил немного анамиции wow js

--- js/scripts.js
+++ js/scripts.js
@@ -111,3 +111,30 @@ $(".js-move-to").on("click", function(e) {
 	});
 
 });
+
+
+/* Добавляем проценты в ширину для элементов блока "Skills" (Навыки) */
+$(".js-add-percent").each(function(idx, _this){
+	$(_this).css({
+		width : $(_this).data("percent")
+	}, 1000);
+});
+
+
+/* Анимации при скролле */
+var wow = new WOW(
+	{
+		boxClass:     'wow',      // animated element css class (default is wow)
+		animateClass: 'animated', // animation css class (default is animated)
+		offset:       100,          // distance to the element when triggering the animation (default is 0)
+		mobile:       true,       // trigger animations on mobile devices (default is true)
+		live:         true,       // act on asynchronously loaded content (default is true)
+		callback:     function(box) {
+			// the callback is fired every time an animation is started
+			// the argument that is passed in is the DOM node being animated
+		},
+		scrollContainer: null // optional scroll container selector, otherwise use window
+	}
+);
+/* инициализация */
+wow.init();
