5ed751c167f3a00d3eebc686a16516c2bf783393
Jamal Hasuev <metafra@mail.ru>
2017-10-17 05:16:45 +0300

    Изменение ссылок в портфолио на битбакет

--- js/portfolios--data.js
+++ js/portfolios--data.js
@@ -4,80 +4,80 @@
 var portfoliosData = [
 	{
 		title : "Арена недвижимости",
-		miniature : "portfolios/real-estate-arena/mini.jpg",
+		miniature : "https://davinskiy.bitbucket.io/portfolios/real-estate-arena/mini.jpg",
 		desc : "Очень красивый макет по услугам подбора квартир в питере - бесплатно. Сверстан на флексах.",
 		impress : "Довольно простой в использовании макет, с понятным функционалом, что может не порадовать. Плюс, довольно адекватный клиент.",
-		url_to_work : "portfolios/real-estate-arena/index.html",
-		url_to_jpg : "portfolios/real-estate-arena/full-img.jpg"
+		url_to_work : "https://davinskiy.bitbucket.io/portfolios/real-estate-arena/index.html",
+		url_to_jpg : "https://davinskiy.bitbucket.io/portfolios/real-estate-arena/full-img.jpg"
 	},
 	{
 		title : "Теплоконтакт",
-		miniature : "portfolios/teplokontakt/mini.jpg",
+		miniature : "https://davinskiy.bitbucket.io/portfolios/teplokontakt/mini.jpg",
 		desc : "Макет строительного характера, представляющий материалы для обеспечение \
 				теплоизоляция домов и других помещений. Был сверстан за 3-4 дня, так как был более чем свободен. \
 				Заказчик попался вполне адекватный, что не могло не порадовать. Верстал на флексах.",
 		impress : "Удобный макет, без каких-либо дизайнерских заморочек, довольно простой при верстке. \
 					Был сверстан, как говорится, на одном дыхание. :) Заказчик был доволен. ",
-		url_to_work : "portfolios/teplokontakt/index.html",
-		url_to_jpg : "portfolios/teplokontakt/full-image.jpg"
+		url_to_work : "https://davinskiy.bitbucket.io/portfolios/teplokontakt/index.html",
+		url_to_jpg : "https://davinskiy.bitbucket.io/portfolios/teplokontakt/full-image.jpg"
 	},
 	{
 		title : "Дождевики",
-		miniature : "portfolios/Rainers/miniature.jpg",
+		miniature : "https://davinskiy.bitbucket.io/portfolios/Rainers/miniature.jpg",
 		desc : "Очень интересный макет лендинга про дождевики. \
 				Где можно выбрать себе подходящий по размеру и по вкусу цвета (черный / белый) дождевик.",
 		impress : "Интересный, сложный, много JS кода, немного не стандартный, вообщем, отличный макет, побольше бы таких :)",
-		url_to_work : "portfolios/Rainers/index.html",
-		url_to_jpg : "portfolios/Rainers/full-img.jpg"
+		url_to_work : "https://davinskiy.bitbucket.io/portfolios/Rainers/index.html",
+		url_to_jpg : "https://davinskiy.bitbucket.io/portfolios/Rainers/full-img.jpg"
 	},
 	{
 		title : "Авто кредит.Редизайн",
-		miniature : "portfolios/auto-credit.redisign/miniature.jpg ",
+		miniature : "https://davinskiy.bitbucket.io/portfolios/auto-credit.redisign/miniature.jpg ",
 		desc : "Очередной макет от постоянного клиента. На этот раз редизайн того макета, \
 				что было сверстана мною же. На этот раз договорились, что верстка будет на флексах.",
 		impress : "По сути такой же макет, что и раньше, разве что по сложности \
 					ощутимо было задний фон видео, что немного украсил однотипную верстку :)",
-		url_to_work : "portfolios/auto-credit.redisign/index.html",
-		url_to_jpg : "portfolios/auto-credit.redisign/full-img.jpg"
+		url_to_work : "https://davinskiy.bitbucket.io/portfolios/auto-credit.redisign/index.html",
+		url_to_jpg : "https://davinskiy.bitbucket.io/portfolios/auto-credit.redisign/full-img.jpg"
 	},
 	{
 		title : "Я ем",
-		miniature : "portfolios/I-eat/mini.jpg ",
+		miniature : "https://davinskiy.bitbucket.io/portfolios/I-eat/mini.jpg ",
 		desc : "Еще один макет от прежней заказчицы. На этот раз немного интересный - про еду :) \
 				Верстал на флексах, клиентка снова была довольна работой :)",
 		impress : "Верстал долго, за 3 дня )) ибо каждый раз, когда открывал макет в фотошопе, я почему-то слышал зов холодильника..",
-		url_to_work : "portfolios/I-eat/index.html",
-		url_to_jpg : "portfolios/I-eat/full-image.png"
+		url_to_work : "https://davinskiy.bitbucket.io/portfolios/I-eat/index.html",
+		url_to_jpg : "https://davinskiy.bitbucket.io/portfolios/I-eat/full-image.png"
 	},
 	{
 		title : "Балтсервис",
-		miniature : "portfolios/BaltService/mini.jpg ",
+		miniature : "https://davinskiy.bitbucket.io/portfolios/BaltService/mini.jpg ",
 		desc : 'Классический макет на тематику "спецтехники и их комплектующие". Где-то интересный, где обычный :)',
 		impress : 'Наверное, только то, что заказчица, которая обратилась с данным макетом, сама довольно неплохо сверстала его. \
 					Но она искала другого испольнителя, так как ее верстка, цитируя, "кривая".',
-		url_to_work : "portfolios/BaltService/index.html",
-		url_to_jpg : "portfolios/BaltService/full-img.jpg"
+		url_to_work : "https://davinskiy.bitbucket.io/portfolios/BaltService/index.html",
+		url_to_jpg : "https://davinskiy.bitbucket.io/portfolios/BaltService/full-img.jpg"
 	},
 	{
 		title : 'GPS Мониторинга под ключ',
-		miniature : 'portfolios/GPS-monitoring/miniature.jpg',
+		miniature : 'https://davinskiy.bitbucket.io/portfolios/GPS-monitoring/miniature.jpg',
 		desc : 'Еще один макет от одного ранего клиента. Про системы GPS мониторинга. \
 				Не совсем понял суть этих мониторингов, но макет довольно интересный. :)) \
 				Сверстал на бутстрапе.',
 		impress : 'Красота в простоте. :) \
 					Но потрудиться все-таки немного пришлось - видеофон на главном экране. :)',
-		url_to_work : 'portfolios/GPS-monitoring/index.html',
-		url_to_jpg : 'portfolios/GPS-monitoring/full-image.jpg'
+		url_to_work : 'https://davinskiy.bitbucket.io/portfolios/GPS-monitoring/index.html',
+		url_to_jpg : 'https://davinskiy.bitbucket.io/portfolios/GPS-monitoring/full-image.jpg'
 	},
 	{
 		title : 'Авто кредит',
-		miniature : 'portfolios/auto-credit/miniature.jpg',
+		miniature : 'https://davinskiy.bitbucket.io/portfolios/auto-credit/miniature.jpg',
 		desc : 'Верстал в то время, когда я немного где-то что-то прочел про БЭМ, \
 				но не корректно понял суть. В этом можно убедиться, увидев классы в коде вида \
 				"how-we-do__steps__step-items__step__dot-line" и "footer__contacts__form__title_overcase-green". \
 				Хотя бы понятно, что где находиться :))',
 		impress : 'Первый заказ, сделанный на бутстрапе, и вроде неплохе справился - заказчик был доволен. :)',
-		url_to_work : 'portfolios/auto-credit/index.html',
-		url_to_jpg : 'portfolios/auto-credit/full-image.jpg'
+		url_to_work : 'https://davinskiy.bitbucket.io/portfolios/auto-credit/index.html',
+		url_to_jpg : 'https://davinskiy.bitbucket.io/portfolios/auto-credit/full-image.jpg'
 	}
 ];
