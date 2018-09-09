c40c3d726e3674b616cc651e84fbf35737d47201
Jamal <mr.davinskiy@mail.ru>
2018-09-09 09:18:47 +0300

    обновил портфолио

--- js/portfolios--data.js
+++ js/portfolios--data.js
@@ -5,60 +5,70 @@ let pDir = 'https://davinskiy.bitbucket.io/portfolios/';
 
 let portfoliosData = [
 	{
+		title : "Fashion Landing",
+		miniature : pDir + "fashion-land/mini.jpg",
+		url_to_work : pDir + "fashion-land/index.html",
+		url_to_jpg : pDir + "fashion-land/full-img.jpg",
+	},
+	{
+		title : "Banks Guarantee",
+		miniature : pDir + "banks-guarantee/mini.jpg",
+		url_to_work : pDir + "banks-guarantee/index.html",
+		url_to_jpg : pDir + "banks-guarantee/full-img.jpg",
+	},
+	{
+		title : "Duty Awards",
+		miniature : pDir + "duty-awards/mini.jpg",
+		url_to_work : pDir + "duty-awards/index.html",
+		url_to_jpg : pDir + "duty-awards/full-img.jpg",
+	},
+	{
 		title : "Echo design",
 		miniature : pDir + "echo-design/mini.jpg",
 		url_to_work : pDir + "echo-design/index.html",
 		url_to_jpg : pDir + "echo-design/full-img.jpg",
-		show: true
 	},
 	{
 		title : "Dental marketing",
 		miniature : pDir + "stomotolog-uv/mini.jpg",
 		url_to_work : pDir + "stomotolog-uv/index.html",
 		url_to_jpg : pDir + "stomotolog-uv/full-img.jpg",
-		show: true
 	},
 	{
 		title : "Лендинг детского магазина",
 		miniature : pDir + "mamka-landing/mini.jpg",
 		url_to_work : pDir + "mamka-landing/index.html",
 		url_to_jpg : pDir + "mamka-landing/full-img.jpg",
-		show: true
 	},
 	{
 		title : "Страница-портфолио",
 		miniature : pDir + "04-portfolio/mini.jpg",
 		url_to_work : pDir + "04-portfolio/index.html",
 		url_to_jpg : pDir + "04-portfolio/full-img.jpg",
-		show: true
 	},
 	{
 		title : "World Of Tanks",
 		miniature : pDir + "wot/mini.jpg",
 		url_to_work : pDir + "wot/index.html",
 		url_to_jpg : pDir + "wot/full-img.jpg",
-		show: true
 	},
 	{
 		title : "Soco Group",
 		miniature : pDir + "soco.group/mini.jpg",
 		url_to_work : pDir + "soco.group/index.html",
 		url_to_jpg : pDir + "soco.group/full-img.jpg",
-		show: true
 	},
 	{
 		title : "Инвестиции",
 		miniature : pDir + "investing/mini.jpg",
 		url_to_work : pDir + "investing/index.html",
 		url_to_jpg : pDir + "investing/full-img.jpg",
-		show: true
 	},
 	{
 		title : "Florida Builders",
 		miniature : pDir + "florida-builders/mini.jpg",
 		url_to_work : pDir + "florida-builders/index.html",
 		url_to_jpg : pDir + "florida-builders/full-img.jpg",
-		show: true
 	},
 	{
 		title : "Mamka Store",
@@ -78,7 +88,7 @@ let portfoliosData = [
 		title : "Правопоритет",
 		miniature : pDir + "pravoparitet/mini.jpg",
 		desc : "Сайт на тему: юридические услуги для организаций и предпринимателей",
-		impress : "[попросили афишировать]",
+		impress : "[попросили не афишировать]",
 		url_to_work : pDir + "pravoparitet/index.html",
 		url_to_jpg : pDir + "pravoparitet/full-img.jpg",
 		show: false
@@ -88,7 +98,6 @@ let portfoliosData = [
 		miniature : pDir + "interior-design/mini.jpg",
 		url_to_work : pDir + "interior-design/index.html",
 		url_to_jpg : pDir + "interior-design/full-img.jpg",
-		show: true
 	},
 	{
 		title : "Ремонт и отделка",
