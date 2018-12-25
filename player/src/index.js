600bebde9d510b4e9a11992c70d0301a7bc1ef89
Jamal <mr.davinskiy@mail.ru>
2018-12-25 21:56:49 +0300

    уже что-то есть, нужно немного доработать...

--- player/src/index.js
+++ player/src/index.js
@@ -1,17 +1,56 @@
-// js
-let video = document.getElementById("myVideo");
+let video_cnt = document.querySelector(".js-video-cnt");
 
-function mediaAutoPlay(obj){
+function playVideo(obj){
+	let video = obj.querySelector('video');
 	video.play();
 	setTimeout(() => {
-		obj.muted = false;
+		video.muted = false;
 	}, 150);
+
+	video.addEventListener("timeupdate", function() {
+		getMediaProgress(this);
+	}, false);
 }
 
-mediaAutoPlay(video);
+playVideo(video_cnt);
 
-video.addEventListener('click',function(){
+function getMediaProgress(video){
+	let cur_time = video.currentTime;
+	let dur = video.duration;
+
+	let percent = (cur_time / dur) * 100;
+
+	let progress_bar = getClosest(video, 'js-video-cnt').querySelector('.js-video-progress');
+
+	// console.log(progress_bar);
+	progress_bar.style.width = percent + '%';
+}
+
+window.videoPause = function(_this){
+	let video = getClosest(_this, 'js-video-cnt').querySelector('video');
 	if(!video.paused){
 		video.pause();
+		video_cnt.classList.add('pause');
 	}
-}, false);
+}
+
+window.videoPlay = function(_this){
+	let video = getClosest(_this, 'js-video-cnt').querySelector('video');
+	if(video.paused){
+		video.play();
+		video_cnt.classList.remove('pause');
+	}
+}
+
+function getClosest(el, className) {
+	let parent = el.parentElement;
+
+	if (parent.nodeName == 'BODY') return;
+	if (hasClass(parent, className)) return parent;
+
+	return getClosest(parent, className);
+}
+
+function hasClass(element, className) {
+	return new RegExp('(\\s|^)' + className + '(\\s|$)').test(element.className);
+}
