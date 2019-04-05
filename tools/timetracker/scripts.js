89bcbac7c27254d05ef0f594a93d36b9469d8fe4
Jamal <mr.davinskiy@mail.ru>
2019-04-05 13:10:57 +0300

    доработал чуть-чуть: убрал стандартные сплывашки и добавил модальки + анимация  + информация о проекте. вроде все

--- tools/timetracker/scripts.js
+++ tools/timetracker/scripts.js
@@ -4,12 +4,28 @@ let tracker_app = new Vue({
 	el : '#tracker-app',
 	data : {
 		tasks : [],
-		rate : 400,
 		statuses : {
 			'light' : 'Еще не начат',
 			'primary' : 'В работе',
 			'secondary' : 'На паузе',
 			'success' : 'Завершен',
+		},
+		statusesVerb : {
+			'primary' : {
+				heading : 'В работу',
+				text_input : 'Начну верстку с хедера и меню...',
+				btn_text : 'Начать работу',
+			},
+			'secondary' : {
+				heading : 'На паузу',
+				text_input : 'Сверстал хедер и меню...',
+				btn_text : 'Поставить на паузу',
+			},
+			'success' : {
+				heading : 'Завершение',
+				text_input : 'Сверстал весь макет, отправляю на согласованию ...',
+				btn_text : 'Завершить',
+			},
 		},
 	},
 	created : function(){
@@ -21,54 +37,64 @@ let tracker_app = new Vue({
 		localStorage.setItem('tracker_app', JSON.stringify(this.tasks));
 	},
 	methods : {
-		getSpendeingTime : function(start, end){
-			if ( !end )
-				end = new Date().getTime();
-			return ((end-start) / (1000*60)).toFixed() + ' мин.';
-		},
-		switchStatus : function(task_id, btn_stat){
+		action : function(){
+			let task_id = this.$refs.confirm_task_id.value*1;
+			let btn_stat = this.$refs.confirm_btn_stat.value;
+			let cmt = this.$refs.confirm_text_input.value;
+
 			let change = true;
-			let cmt;
 			let times_last_index = this.tasks[task_id].times.length-1;
 
 			switch(btn_stat) {
 				case 'primary' /*В работу*/:
 					this.tasks[task_id].times.push({
-						title : prompt('Какая задача стоит?'),
+						title : cmt,
 						start : new Date().getTime(),
 						end : 0,
 						comment : '',
 					});
 					break;
+
 				case 'secondary' /*На паузу*/:
-					cmt = prompt('Хотите добавить комментарий к действию?');
-					
-					if (!cmt) {
-						change = false;
-						break;
-					}
-					
 					this.tasks[task_id].times[times_last_index].comment = cmt;
 					this.tasks[task_id].times[times_last_index].end = new Date().getTime();
 					break;
-				case 'success' /*Завершение*/:
-					cmt = prompt('Хотите добавить/обновить комментарий к действию?', this.tasks[task_id].times[times_last_index].comment);
-					
-					if (!cmt) {
-						change = false;
-						break;
-					}
 
+				case 'success' /*Завершение*/:
 					this.tasks[task_id].times[times_last_index].comment = cmt;
 
 					/* делаем проверку, на случае если перед этим работа была приостановлена */
 					if (!this.tasks[task_id].times[times_last_index].end)
 						this.tasks[task_id].times[times_last_index].end = new Date().getTime();
 					break;
+
+				default: 
+					change = false;
 			}
-			
-			if(change)
+
+			if (change) {
 				this.tasks[task_id].status = btn_stat;
+				event.target.reset();
+				modalClose();
+			}
+		},
+		switchStatus : function(task_id, btn_stat){
+
+			window.modalOpen('', '#' + 'confirm-edit');
+
+			this.$refs.confirm_heading.innerText = this.statusesVerb[btn_stat].heading;
+			this.$refs.confirm_btn_text.innerText = this.statusesVerb[btn_stat].btn_text;
+
+			this.$refs.confirm_btn_stat.value = btn_stat;
+			this.$refs.confirm_task_id.value = task_id;
+
+			this.$refs.confirm_text_input.setAttribute('placeholder', this.statusesVerb[btn_stat].text_input);
+
+			if (btn_stat == 'success') {
+				this.$refs.confirm_text_input.value = this.tasks[task_id].times[this.tasks[task_id].times.length-1].comment;
+			}
+
+
 		},
 		addProject : function(){
 			
@@ -84,6 +110,7 @@ let tracker_app = new Vue({
 				title : title,
 				desc : desc,
 				rate : rate,
+				paid : 0,
 				status : 'light',
 				show_times : false,
 				times : [],
@@ -92,10 +119,18 @@ let tracker_app = new Vue({
 			modalClose();
 			this.$refs.new_title.form.reset();
 		},
-		remove : function(task_id){
-			if (confirm('Вы действительно хотите удалить проект из списка?')) {
-				this.tasks.splice(task_id, 1);
-			}
+		remove : function(){
+			let task_id = this.$refs.task_id.value;
+			this.tasks.splice(task_id, 1);
+
+			modalClose();
+			this.$refs.new_title.form.reset();
+		},
+		removeConfirm : function(task_id){
+
+			window.modalOpen('', '#' + 'confirm-remove');
+			this.$refs.remove_task_title.innerText = this.tasks[task_id].title;
+			this.$refs.task_id.value = task_id;
 		},
 		getProcessInfo : function(task){
 			let all_ts = 0;
@@ -115,27 +150,118 @@ let tracker_app = new Vue({
 			let money = (hours * rate).toFixed();
 			
 			return {
-				'minutes' : minutes.toFixed(2),
-				'hours' : hours.toFixed(2),
+				'time' : this.getPassedTime(all_ts),
 				'money' : money,
 			};
 		},
 		getStringifyDate : function(ts){
 			let d = new Date(ts);
-			let s = ((d.getDate() < 10)?'0':'') + d.getDate();
-				s += '.';
-				s += (((d.getMonth()+1) < 10)?'0':'') + (d.getMonth()+1);
-				s += '.';
-				s += ((d.getFullYear() < 10)?'0':'') + d.getFullYear();
-				s += ' в ';
+			let s = '';
 				s += ((d.getHours() < 10)?'0':'') + d.getHours();
 				s += ':';
 				s += ((d.getMinutes() < 10)?'0':'') + d.getMinutes();
-				s += ':';
-				s += ((d.getSeconds() < 10)?'0':'') + d.getSeconds();
+				s += ' в ';
+				s += ((d.getFullYear() < 10)?'0':'') + d.getFullYear();
+				s += '.';
+				s += ((d.getDate() < 10)?'0':'') + d.getDate();
+				s += '.';
+				s += (((d.getMonth()+1) < 10)?'0':'') + (d.getMonth()+1);
 
 			return s;
 		},
+		getPassedTime : function(ts){
+			if (!ts) return null;
+
+			let hours = parseInt(ts / (60 * 60 * 1000));
+			let minutes = parseInt(ts / (60 * 1000)) % 60;
+			let seconds = parseInt(ts / (1000)) % 60;
+			
+			let time_arr = [];
+
+			if (hours)
+				time_arr.push(hours + 'ч');
+			if (minutes)
+				time_arr.push(minutes + 'м');
+			if (seconds)
+				time_arr.push(seconds + 'с');
+			
+			return time_arr.join(', ');
+		},
+
+		getHeight : function(el) {
+			var el_style      = window.getComputedStyle(el),
+				el_display    = el_style.display,
+				el_position   = el_style.position,
+				el_visibility = el_style.visibility,
+				el_max_height = el_style.maxHeight.replace('px', '').replace('%', ''),
+
+				wanted_height = 0;
+
+
+			// if its not hidden we just return normal height
+			if(el_display !== 'none' && el_max_height !== '0') {
+				return el.offsetHeight;
+			}
+
+			// the element is hidden so:
+			// making the el block so we can meassure its height but still be hidden
+			el.style.position   = 'absolute';
+			el.style.visibility = 'hidden';
+			el.style.display    = 'block';
+
+			wanted_height     = el.offsetHeight;
+
+			// reverting to the original values
+			el.style.display    = el_display;
+			el.style.position   = el_position;
+			el.style.visibility = el_visibility;
+
+			return wanted_height;
+		},
+		toggleSlide : function(el, dur) {
+
+			let height = 0;
+				dur = dur || 300;
+			
+			el.style.overflow = 'hidden';
+
+			el.style['transition'] = 'max-height '+ dur/1000 +'s ease-in-out';
+			
+			if (getComputedStyle(el).display == 'none') {
+				height = this.getHeight(el);
+				el.style.maxHeight = '0px';
+				el.style.display = 'block';
+			} else {
+				el.style.maxHeight = el.scrollHeight + 'px';
+			}
+
+			setTimeout(function(){
+				el.style.maxHeight = height + 'px';
+			}, 10);
+
+			setTimeout(function(){
+
+				el.style.overflow = '';
+
+				if (el.style.maxHeight == '0px') {
+					el.style.display = 'none';
+				}
+				el.style.maxHeight = '';
+
+			}, dur+1);
+
+		},
+		toggleTimes : function(e, box){
+			let this_btn = e.target;
+
+			this.toggleSlide(this.$refs[box][0]);
+
+			if (this_btn.getAttribute('data-replace-text') == this_btn.innerText) {
+				this_btn.innerText = this_btn.getAttribute('data-orgin-text');
+			} else {
+				this_btn.innerText = this_btn.getAttribute('data-replace-text');
+			}
+		}
 	}
 });
 
@@ -168,4 +294,9 @@ function modalClose(event){
 	document.querySelectorAll(".modals, .modals__item").forEach(function(element){
 		element.classList.remove('show');
 	});
+
+	document.querySelectorAll(".modals form").forEach(function(element){
+		element.reset();
+	});
+
 };
