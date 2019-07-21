b53b777ca3cb5db04f1006812d1c3bae9f7895a6
Jamal <mr.davinskiy@mail.ru>
2019-07-21 22:05:54 +0300

    super piper note

---
+++ tools/super-puper-note/sciprts.js
@@ -0,0 +1,38 @@
+new Vue({
+	el : '.note',
+	data : {
+		create_title : '',
+		create_text : '',
+		notes : [],
+	},
+	watch : {
+		notes : function() {
+			this.save();
+		}
+	},
+	created : function() {
+		if (localStorage.getItem('notes') != '') {
+			this.notes = JSON.parse(localStorage.getItem('notes'));
+		}
+	},
+	methods : {
+		remove : function(indx) {
+			this.notes.splice(indx, 1);
+		},
+		add : function() {
+			if (!this.notes)
+				this.notes = [];
+
+			this.notes.unshift({
+				title : this.create_title,
+				text : this.create_text,
+				is_edit : false
+			});
+			this.create_title = '';
+			this.create_text = '';
+		},
+		save : function() {
+			localStorage.setItem('notes', JSON.stringify(this.notes));
+		},
+	},
+});
