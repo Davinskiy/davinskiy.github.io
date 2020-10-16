756eac7dc4176e564aad521ec00f2bee6d3e9317
Jamal Hasuev <metafra@mail.ru>
2020-10-16 21:11:09 +0300

    test-task | LeadHit | building

---
+++ test-tasks/5/dist/js/chunk-2d215fa4.13df1b9c.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d215fa4"],{c100:function(t,n,e){"use strict";e.r(n);var u=function(){var t=this,n=t.$createElement,e=t._self._c||n;return e("i")},o=[],c={beforeMount:function(){this.$store.commit("logout"),this.$router.push({name:"auth"})}},r=c,s=e("2877"),i=Object(s["a"])(r,u,o,!1,null,null,null);n["default"]=i.exports}}]);
+//# sourceMappingURL=chunk-2d215fa4.13df1b9c.js.map
