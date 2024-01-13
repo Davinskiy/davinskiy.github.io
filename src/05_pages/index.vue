11f32500b62e1a6f37f1975cd84790109c0fcec5
Jamal Hasuev <jhasuev@mail.ru>
2024-01-13 20:39:10 +0300

    add top-header

--- src/05_pages/index.vue
+++ src/05_pages/index.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
+import TopHeader from '@/03_features/top-header.vue'
+
 defineOptions({ name: 'Pages' })
 </script>
 
 <template>
   <div>
+    <TopHeader />
     <RouterView />
   </div>
 </template>
